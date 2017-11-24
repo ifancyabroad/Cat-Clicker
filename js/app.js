ab40bd4d78506a95760f0158994091ed1827d7d8
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-11-24 22:11:47 +0000

    refactor: Added comments to JS code

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
-const catsDisplay = document.getElementById('cats-display');
+// Array containing all cat names and images
+// Add new cats here
 let catData = [
 {name: 'Bob',
 image: 'images/cat.jpg'},
 {name: 'Henry',
 image: 'images/cat2.jpg'}
 ];
+
+// Array for all cat instances to be stored
 let catArray = [];
 
+// Cat class
 class Cat {
 	constructor(id, name, image) {
+		// Id to match cat instance with user selection
 		this.id = id;
+		// Cat properties
 		this.name = name;
 		this.image = image;
 		this.clicks = 0;
@@ -22,16 +28,19 @@ class Cat {
 			<img src="${this.image}" alt="A picture of a cat" class="cat-picture">
 		</div>`
 	}
+	// Method for incrementing clicks to the cat
 	addClick(display) {
 		this.clicks++;
 		display.innerHTML = 'Clicks: ' + this.clicks;
 	}
+	// Method for adding cat content to the page
 	addContent() {
+		const catsDisplay = document.getElementById('cats-display');
 		catsDisplay.insertAdjacentHTML('beforeend', this.html);
 	}
 }
 
-
+// Function for creating all cats in the catData array
 const createCats = function() {
 	let id = 0;
 	catData.forEach(function(cat) {
@@ -42,13 +51,17 @@ const createCats = function() {
 	});
 }();
 
+// Object containing all cat pictures on the page
 const catPicture = document.getElementsByClassName('cat-picture');
 
+// Adds an event listener to each picture
 for (let cat of catPicture) {
 	cat.addEventListener('click', function(e) {
+		// Find out which cat instance has been selected
 		let choice = e.target.parentElement.id;
 		let counter = e.target.previousElementSibling.lastElementChild;
 		catArray.forEach(function(cat) {
+			// Increment the relevant cat counter
 			if (choice == cat.id) {
 				cat.addClick(counter);
 			}
