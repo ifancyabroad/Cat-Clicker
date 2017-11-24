4613ba5835061b599f680e62e133a2a2987d45d4
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-11-24 18:37:29 +0000

    feat: Clicking the cat now increments the clicks element

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const catPicture = document.getElementById('cat-picture');
+const clickDisplay = document.getElementsByTagName('h2')[0];
+let clicks = 0;
 
 catPicture.addEventListener('click', function(){
-
-
+	clicks++;
+	clickDisplay.innerHTML = 'Clicks: ' + clicks;
 }, false);
