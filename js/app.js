068639a5fcdeda4ffe01b6bda26f867b51f7c23e
Edgar Nightingale <edgar.nightingale@btinternet.com>
2017-11-24 20:28:21 +0000

    refactor: Created cat class

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
+const catsDisplay = document.getElementById('cats-display');
 const catPicture = document.getElementById('cat-picture');
-const clickDisplay = document.getElementsByTagName('h2')[0];
-let clicks = 0;
+const clickDisplay = document.getElementsById('clicks');
+
+class Cat {
+	constructor(name, image) {
+		this.name = name;
+		this.image = image;
+		this.clicks = 0;
+		this.html = 
+		`<div class="cat">
+			<ul class="cat-details">
+				<li id="cat-name">${this.name}</li>
+				<li id="clicks">Clicks: 0</li>
+			</ul>
+			<img src="${this.image}" alt="A picture of a cat" id="cat-picture">
+		</div>`
+	}
+	addClick() {
+		this.clicks++;
+	}
+	addContent() {
+		catsDisplay.insertAdjacentHTML('beforeend', this.html);
+	}
+}
+
+const cat1 = new Cat('Bob', 'images/cat.jpg');
+const cat2 = new Cat('Henry', 'images/cat1.jpg');
+
+cat1.addContent();
+cat2.addContent();
 
 catPicture.addEventListener('click', function(){
-	clicks++;
 	clickDisplay.innerHTML = 'Clicks: ' + clicks;
 }, false);
